24257ba98ee0b7771aae1cb72377158b11b0968a
Sam <85004512+aar2dee2@users.noreply.github.com>
2021-09-30 14:30:13 +0000

    added source for icons from fontsawesome to all html templates

--- chirp/lib/chirp_web/live/post_live/post_component.ex
+++ chirp/lib/chirp_web/live/post_live/post_component.ex
@@ -3,6 +3,7 @@ defmodule ChirpWeb.PostLive.PostComponent do
 
   def render(assigns) do
     ~L"""
+    <script src="https://kit.fontawesome.com/d07169d6d4.js" crossorigin="anonymous"></script>
     <div id="post-<%= @post.id %>" class="post">
       <div class="row">
         <div class="column column-10">
@@ -18,20 +19,20 @@ defmodule ChirpWeb.PostLive.PostComponent do
       <div class="row">
         
         <div class="column">
-          <i class="far fa-heart"></i> <%= @post.likes_count %>
+          <i class="fa fa-heart"></i> <%= @post.likes_count %>
         </div>
         <div class="column">
-          <i class="far fa-retweet"></i> <%= @post.reposts_count %>
+          <i class="fa fa-retweet"></i> <%= @post.reposts_count %>
         </div>
 
         <div class="column">
           
           <%= live_patch to: Routes.post_index_path(@socket, :edit, @post.id) do %>
-            <i class="far fa-edit"></i>
+            <i class="fa fa-edit"></i>
           <% end %>
 
           <%= link to: "#", phx_click: "delete", phx_value_id: @post.id do%>
-            <i class="far fa-trash-alt"></i>
+            <i class="fa fa-trash-alt"></i>
           <% end %>
 
         </div>
